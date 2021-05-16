e941dbe03d06759633f49e396f4bbe3a4ba5c248
KonghaYao <20192831006@m.scnu.edu.cn>
2021-05-16 19:11:35 +0800

    2021/5/16

--- utils/saveImg.js
+++
@@ -1,8 +0,0 @@
-const fse = require("fs-extra");
-const fetch = require("node-fetch");
-function saveImg(path, savePath) {
-    return fetch(path)
-        .then((res) => res.buffer())
-        .then((res) => fse.outputFile(savePath, res));
-}
-module.exports = saveImg;
